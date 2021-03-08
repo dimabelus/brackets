a8277498e17fcbab9fc672750e8af92207764a23
dimabelus <dima21belus@gmail.com>
2021-03-08 20:46:33 +0300

    feat: implement brackets patterns matching check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  // Set environment variable equal to str length
+  let strLength = str.length;
+  // Set environment variable equal to bracketsConfig length
+  let bracketsConfigLength = bracketsConfig.length;
+
+  // Iter amount of iterations
+  for (let i = 0; i < strLength; i++) {
+    // Iterate over elements of bracketsConfig
+    for (let j = 0; j < bracketsConfigLength; j++) {
+      // Set current brackets pair through concantenation
+      // of the current index of j[0] and i[1]
+      let currentBracketsPair = bracketsConfig[j][0] + bracketsConfig[j][1];
+      // Check if current brackets pair exists in str
+      if (str.includes(currentBracketsPair)) {
+        // Replace matching pair with an empty string
+        str = str.replace((currentBracketsPair), '');
+      }
+    }
+  }
+  // If nothing left return true
+  if (str === '') {
+    return true
+  // If any unpaired symbols left return false
+  } else {
+    return false
+  }
+};
